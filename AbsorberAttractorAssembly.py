7436f8720947658488108e1736c90a27b4f56457
Joseph Howlett <JosephJHowlett@gmail.com>
2023-07-06 11:14:18 -0700

    updates to distance scanning

--- AbsorberAttractorAssembly.py
+++ AbsorberAttractorAssembly.py
@@ -1,20 +1,22 @@
 from MDT693B import MDT693B
 import numpy as np
 
-volts_per_micron = 150.0/1150  # using available stroke of piezo
-two_piezo_distance_cm = 9.128  # from SW model
 
 class AbsorberAttractorAssembly:
-    def __init__(self, controller_port):
+    def __init__(self, controller_port, **kwargs):
         self.piezo_controller = MDT693B(controller_port)
+        self.volts_per_micron = kwargs.get('volts_per_micron', 150.0/1150)  # using available stroke of piezo
+        self.two_piezo_distance_cm = kwargs.get('two_piezo_distance_cm', 9.128)  # from SW model
         self.matrices = dict(
-            theta=-1 * np.sqrt(3)/4 * two_piezo_distance_cm * 1e4 * np.array([1.0, -0.5, -0.5]),
-            phi=two_piezo_distance_cm * 1e4 * np.array([0., -0.5, 0.5])
+            theta=-1 * np.sqrt(3)/4 * self.two_piezo_distance_cm * 1e4 * np.array([1.0, -0.5, -0.5]),
+            phi=self.two_piezo_distance_cm * 1e4 * np.array([0., -0.5, 0.5])
         )
         return
 
-    def set_voltages(self, voltages):
-        _ = self.piezo_controller.set_voltages(voltages)
+    def set_voltages(self, voltage):
+        if not hasattr(voltage, '__len__'):
+            voltage = np.array([voltage]*3)
+        _ = self.piezo_controller.set_voltages(voltage)
         assert (_==[1,1,1]),"voltage setting failed"
         return
 
@@ -26,7 +28,7 @@ class AbsorberAttractorAssembly:
             start_voltage = self.get_voltages()
         matrix = self.matrices[axis]
         distance_adjustment = matrix * np.sin(angle)
-        voltage_adjustments = -1 * matrix * np.sin(angle) * volts_per_micron
+        voltage_adjustments = -1 * matrix * np.sin(angle) * self.volts_per_micron
         voltages = start_voltages + voltage_adjustments
-        piezo_controller.set_voltages(voltages)
+        self.set_voltages(voltages)
         return
--- scripts/scan_distance.py
+++ scripts/scan_distance.py
@@ -7,57 +7,61 @@ from tqdm import tqdm
 from AH2550A import AH2550A
 from AbsorberAttractorAssembly import *
 
-micron_per_cm = 1e6/1e2
-cap_to_sep = lambda c: 50*140/c  # very rough --> get real numbers from Albert
-
 piezo_controller_port = 'COM3'
 bridge_resource_name = 'GPIB0::28::INSTR'
 
-basename = '20230705_voltagescan_1Vstep'
-
+if (AAA in locals()) or (AAA in globals()):
+    AAA.piezo_controller.close()
 AAA = AbsorberAttractorAssembly(piezo_controller_port)
-
 bridge = AH2550A(bridge_resource_name, timeout=1e4)
 
 stepsize = 1  # Volts
 samples_per_voltage = 2
 
-voltages, step = np.linspace(0, 150, int(150*(1/stepsize)) + 1, retstep=True)
-assert step==stepsize, "something wrong with linspace"
+for stepsize, samples_per_voltage in zip(
+        (1, 1, 0.1, 0.1, 0.01),
+        (2, 2, 5, 5, 10),
+    ):
+    basename = '{dt}_voltagescan_{stepsize}Vstep'.format(
+        dt=time.strftime('%Y%m%d_%H%M%S'),
+        stepsize=stepsize,
+    )
 
+    voltages, step = np.linspace(0, 150, int(150*(1/stepsize)) + 1, retstep=True)
+    assert step==stepsize, "something wrong with linspace"
 
-for direction in ('decreasing', 'increasing'):
-    voltages = voltages[::-1]
-    outfilebase = basename + f'_{direction}V'
-    outdata = []
-    for i in tqdm(range(len(voltages))):
-        AAA.set_voltages(np.array([voltages[i]]*3))
-        time.sleep(0.1)
-        try:
-            c, l, v = bridge.n_measurements(samples_per_voltage, max_attempts=100)
-        except IndexError as e:
-            print(e)
-            print('Ending loop early')
-            break
-        outdict = dict(
-            voltage=voltages[i],
-            cmean=c.mean(),
-            cstd=c.std(),
-            lmean=l.mean(),
-            lstd=l.std(),
-            vmean=v.mean(),
-            vstd=v.std(),
-        )
-        print(outdict)
-        outdata.append(outdict)
-        with open(outfilebase+'.dat', 'a') as f:
-            f.write(' '.join(map(str, list(outdict.values()))) + '\n')
-    df = pd.DataFrame(outdata)
-    df.to_csv(outfilebase+'.csv')
-    plt.plot(df.voltage, 1.0/df.cmean, '.', label=f'{direction} voltage')
+    for direction in ('decreasing', 'increasing'):
+        voltages = voltages[::-1]
+        outfilebase = basename + f'_{direction}V'
+        outdata = []
+        for i in tqdm(range(len(voltages))):
+            AAA.set_voltages(voltages[i])
+            time.sleep(0.1)
+            try:
+                c, l, v = bridge.n_measurements(samples_per_voltage, max_attempts=100)
+            except IndexError as e:
+                print(e)
+                print('Ending loop early')
+                break
+            outdict = dict(
+                voltage=voltages[i],
+                cmean=c.mean(),
+                cstd=c.std(),
+                lmean=l.mean(),
+                lstd=l.std(),
+                vmean=v.mean(),
+                vstd=v.std(),
+            )
+            #print(outdict)
+            outdata.append(outdict)
+            with open(outfilebase+'.dat', 'a') as f:
+                f.write(' '.join(map(str, list(outdict.values()))) + '\n')
+        df = pd.DataFrame(outdata)
+        df.to_csv(outfilebase+'.csv')
+        plt.plot(df.voltage, 1.0/df.cmean, '.', label=f'{direction} voltage')
 
-plt.xlabel('piezo voltage')
-plt.ylabel('1/C [pF-1]')
-plt.legend()
-plt.savefig(basename + '.png')
-plt.show()
+    plt.xlabel('piezo voltage')
+    plt.ylabel('1/C [pF-1]')
+    plt.legend()
+    plt.savefig(basename + '.png')
+    #plt.show()
